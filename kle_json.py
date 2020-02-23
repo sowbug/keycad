871250e1a74f0c9403cac41ad1bdab3100b370ca
Mike Tsao <mike@sowbug.com>
2020-02-22 23:04:31 -0800

    start testing parser

--- kle_json.py
+++ kle_json.py
@@ -1,40 +1,55 @@
 import json
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 
 class Parser:
     def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.__key_count = 0
         self.__row_index = 0
         self.__col_index = 0
 
-    def process_row_metadata(self, metadata):
+    @property
+    def key_count(self):
+        return self.__key_count
+
+    def _process_row_metadata(self, metadata):
         pass
 
-    def process_key_metadata(self, metadata):
+    def _process_key_metadata(self, metadata):
         if 'w' in metadata:
             pass
             #self.__pcb.set_logical_key_width(float(metadata['w']))
 
-    def process_key(self, key):
-        pass
+    def _process_key(self, key):
+        logger.info("processing key '%s'" % (key))
+        self.__key_count += 1
 
-    def process_row(self, row):
+    def _process_row(self, row):
         self.__col_index = 0
         for key in row:
             if isinstance(key, dict):
-                self.process_key_metadata(key)
+                self._process_key_metadata(key)
             else:
-                self.process_key(key)
+                self._process_key(key)
                 self.__col_index += 1
                 #self.__pcb.advance_cursor()
 
-    def handle_json(self, json):
-        for row in json:
+    def handle_dict(self, kle_dict):
+        for row in kle_dict:
             if isinstance(row, dict):
-                self.process_row_metadata(row)
+                self._process_row_metadata(row)
             else:
-                self.process_row(row)
+                self._process_row(row)
                 self.__row_index += 1
                 #self.__pcb.cursor_carriage_return()
 
     def load(self, filename):
+        self.reset()
         with open(filename, "r") as f:
-            self.handle_json(json.loads(f.read()))
+            self.handle_dict(json.loads(f.read()))
---
+++ kle_json_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+import kle_json
+
+class TestKleJson(unittest.TestCase):
+
+    def test_simple(self):
+        p = kle_json.Parser()
+
+        p.handle_dict({})
+        self.assertEqual(p.key_count, 0)
+
+        p.handle_dict([["0", "1"], ["2", "3"]])
+        self.assertEqual(p.key_count, 4)
+    
+    def test_sample_files(self):
+        SAMPLES = {
+            "planck-default": {'key_count': 47},
+            "68keys-io": {'key_count': 68},
+            "number-pad": {'key_count': 17},
+        }
+        
+        p = kle_json.Parser()
+        for basename, s in SAMPLES.items():
+            p.load("kle_layouts/%s.json" % (basename))
+            self.assertEqual(p.key_count, s['key_count'])
+
+if __name__ == "__main__":
+    unittest.main()
